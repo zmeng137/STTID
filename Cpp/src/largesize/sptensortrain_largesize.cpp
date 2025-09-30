9e88fac5b7347aa6620c0e450fd7c336d93aa430
MengZhaonan1998 <mengzn1998@gmail.com>
2025-09-29 23:21:24 -0400

    support cross format or ID format

--- Cpp/src/largesize/sptensortrain_largesize.cpp
+++ Cpp/src/largesize/sptensortrain_largesize.cpp
@@ -35,13 +35,15 @@ TT_ID_sparse(const COOTensor<T, Order>& tensor, double const cutoff, size_t cons
     // Initialize a result list, start TT iteration
     SparseTTRes ResList;
     ResList.InterG.resize(dim - 2);
+    ResList.InterC.resize(dim - 1);
+
     for (int i = dim - 1; i > 0; i--) {
         std::cout << "Tensor train iteration " << i << " starts..." << std::endl;
 
         // Reshape matrix (skip the first iteration)
         if (i != dim - 1) {
-            row = nbar / r / shape[i]; // Matrix row
-            col = r * shape[i];        // Matrix column
+            row = nbar / r / shape[i];  // Matrix row
+            col = r * shape[i];         // Matrix column
             W.reshape(row, col);
         }
 
@@ -52,39 +54,69 @@ TT_ID_sparse(const COOTensor<T, Order>& tensor, double const cutoff, size_t cons
         auto idResult = dSparse_Interpolative_CPU_l3(W, cutoff, spthres, r_max, cross_flag);
         #endif
 
-        // There is no cutoff selection. Rank is revealed automatically by IDQR
+        // There is no cutoff selection. Rank is revealed automatically by ID-PRRLU
         size_t ri = idResult.output_rank;
 
-        auto Z = dcoeffZReconCPU(idResult.sparse_interp_coeff, idResult.cps, ri, col);
-
-       // Print the low rank approximation error and sparse information
+        // Output
         if (cross_flag) {
+            // TT-core and pivot matrix
             auto row_subset = W.subrow(idResult.pivot_rows, ri);
-            if (i == dim - 1)
-                outFile << "CROSS (two-side ID) Format TT-factor (i.e. Row skeleton)" << std::endl;
-            outFile << "Mode " << i << " Core [" << ri << ", " << shape[i] << ", " << r << "] NNZs: " << row_subset.nnz_count << std::endl; 
-        } else {
-            if (i == dim - 1)
-                outFile << "ID (one-side ID) Format TT-factor (i.e. Interpolation)" << std::endl;
-            outFile << "Mode " << i << " NNZs: " << Z.nnz_count << std::endl;
-        }
-
-        // Form a new tensor-train factor
-        if (i == dim - 1) {
-            ResList.EndG.reset(Z.nnz_count, ri, shape[i]);
-            ResList.EndG.nnz_count = Z.nnz_count;
-            std::copy(Z.values, Z.values + Z.nnz_count, ResList.EndG.values);
-            std::copy(Z.row_indices, Z.row_indices + Z.nnz_count, ResList.EndG.indices[0]);
-            std::copy(Z.col_indices, Z.col_indices + Z.nnz_count, ResList.EndG.indices[1]);
-        } else {
-            ResList.InterG[i - 1].reset(Z.nnz_count, ri, shape[i], r);
-            ResList.InterG[i - 1].nnz_count = Z.nnz_count;
-            std::copy(Z.values, Z.values + Z.nnz_count, ResList.InterG[i - 1].values);
-            std::copy(Z.row_indices, Z.row_indices + Z.nnz_count, ResList.InterG[i - 1].indices[0]);
-            for (size_t n = 0; n < Z.nnz_count; ++n) {
-                ResList.InterG[i - 1].indices[1][n] = Z.col_indices[n] / r;
-                ResList.InterG[i - 1].indices[2][n] = Z.col_indices[n] % r;
+            auto pivot_mat = row_subset.subcol(idResult.pivot_cols, ri);
+            
+            // TT-cores
+            if (i == dim - 1) {
+                outFile << "TT representation (TCI format)" << std::endl;
+                ResList.EndG.reset(row_subset.nnz_count, ri, shape[i]);
+                ResList.EndG.nnz_count = row_subset.nnz_count;
+                std::copy(row_subset.values, row_subset.values + row_subset.nnz_count, ResList.EndG.values);
+                std::copy(row_subset.row_indices, row_subset.row_indices + row_subset.nnz_count, ResList.EndG.indices[0]);
+                std::copy(row_subset.col_indices, row_subset.col_indices + row_subset.nnz_count, ResList.EndG.indices[1]);
+            } else {
+                ResList.InterG[i-1].reset(row_subset.nnz_count, ri, shape[i], r);
+                ResList.InterG[i-1].nnz_count = row_subset.nnz_count;
+                std::copy(row_subset.values, row_subset.values + row_subset.nnz_count, ResList.InterG[i-1].values);
+                std::copy(row_subset.row_indices, row_subset.row_indices + row_subset.nnz_count, ResList.InterG[i-1].indices[0]);
+                for (size_t n = 0; n < row_subset.nnz_count; ++n) {
+                    ResList.InterG[i-1].indices[1][n] = row_subset.col_indices[n] / r;
+                    ResList.InterG[i-1].indices[2][n] = row_subset.col_indices[n] % r;
+                }
             }
+            
+            // Pivot matrices
+            ResList.InterC[i-1].reset(pivot_mat.nnz_count, ri, ri);
+            ResList.InterC[i-1].nnz_count = pivot_mat.nnz_count;
+            std::copy(pivot_mat.values, pivot_mat.values + pivot_mat.nnz_count, ResList.InterC[i-1].values);
+            std::copy(pivot_mat.row_indices, pivot_mat.row_indices + pivot_mat.nnz_count, ResList.InterC[i-1].indices[0]);
+            std::copy(pivot_mat.col_indices, pivot_mat.col_indices + pivot_mat.nnz_count, ResList.InterC[i-1].indices[1]);
+
+            // Output NNZ info
+            outFile << "Mode " << i << " TT-Core [" << ri << ", " << shape[i] << ", " << r << "] NNZs: " << row_subset.nnz_count << std::endl;
+            outFile << "Mode " << i << " Piv mat [" << ri << ", " << ri << "] NNZs: " << pivot_mat.nnz_count << std::endl; 
+        } else {
+            // Inverse merge kernel for reconstruction approximant
+            auto Z = dcoeffZReconCPU(idResult.sparse_interp_coeff, idResult.cps, ri, col);
+
+            // TT-cores
+            if (i == dim - 1) {
+                outFile << "TT representation (one-side ID format)" << std::endl;
+                ResList.EndG.reset(Z.nnz_count, ri, shape[i]);
+                ResList.EndG.nnz_count = Z.nnz_count;
+                std::copy(Z.values, Z.values + Z.nnz_count, ResList.EndG.values);
+                std::copy(Z.row_indices, Z.row_indices + Z.nnz_count, ResList.EndG.indices[0]);
+                std::copy(Z.col_indices, Z.col_indices + Z.nnz_count, ResList.EndG.indices[1]);
+            } else {
+                ResList.InterG[i-1].reset(Z.nnz_count, ri, shape[i], r);
+                ResList.InterG[i-1].nnz_count = Z.nnz_count;
+                std::copy(Z.values, Z.values + Z.nnz_count, ResList.InterG[i-1].values);
+                std::copy(Z.row_indices, Z.row_indices + Z.nnz_count, ResList.InterG[i-1].indices[0]);
+                for (size_t n = 0; n < Z.nnz_count; ++n) {
+                    ResList.InterG[i-1].indices[1][n] = Z.col_indices[n] / r;
+                    ResList.InterG[i-1].indices[2][n] = Z.col_indices[n] % r;
+                }
+            }
+            
+            // Output NNZ info    
+            outFile << "Mode " << i << " TT-Core [" << ri << ", " << shape[i] << ", " << r << "] NNZs: " << Z.nnz_count << std::endl;
         }
 
         // Form new W from the interpolative factor C
